dbd8cf0d95c75364be8f4b2a12cb61d31df21859
李阳阳 <yangyangli@hefujituan.com>
2022-08-30 19:11:04 +0800

    发布1.0.1版本

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # auto-generate-template
 auto generate template file such as vue3、vue2 or react
+# 功能介绍
+自动生成vue2,vue3，react模板文件，省去开发时创建文件的烦恼
+可以根据需求在template文件中自行更改
+# 使用说明
+ npm i auto-generate-template -D
 
-# 将文件放在项目根目录下
+ 在package.json->scripts中添加命令 'generate'：'node_modules/auto-generate-template'
 
-# 在package.json->scripts中添加命令 'generate'：'./auto-generate-template/index'
-
-# npm run generate
+ npm run generate
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve' // 依赖引用插件
+import commonjs from 'rollup-plugin-commonjs' // commonjs模块转换插件
+import babel from 'rollup-plugin-babel' // babel 插件
+
+export default {
+  input: './src/index.js', // 打包的入口文件
+  output: {
+    name: 'atuo-generate-template', // 输入的包名
+    file: './bin/index.js', // 打包输出地址, 这里的导出地址就是package内main的地址
+    format: 'umd' // 包类型
+  },
+  plugins: [
+    // 使用的插件
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true
+    }),
+  ],
+  ignore: [
+    'node_modules/**' // 忽略目录
+  ]
+}
