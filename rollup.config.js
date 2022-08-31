2b4279043007255bb8a81c925c73cb100a632e92
李阳阳 <yangyangli@hefujituan.com>
2022-08-31 10:29:57 +0800

    1.0.5增加命令行工具

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default {
   input: './src/index.js', // 打包的入口文件
   output: {
     name: 'atuo-generate-template', // 输入的包名
-    file: './bin/index.js', // 打包输出地址, 这里的导出地址就是package内main的地址
+    file: '/index.js', // 打包输出地址, 这里的导出地址就是package内main的地址
     format: 'umd' // 包类型
   },
   plugins: [
